11f3a7add34f0103be790e92bcafd79e4a047d09
Ferdouse Binte Asad <ferdousebinteasad@gmail.com>
2023-01-07 19:46:09 +0600

    commit

---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Card.css';
+
+const Card = () => {
+  return (
+    <>
+      <div className="card">
+        <div className="inner-card">
+            
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
